6bbd28476b5517e6d8b71e09455b419a0e6aab8e
Rahul <ranarahul41178@gmail.com>
2024-08-07 22:39:24 +0530

    initial commit

---
+++ 31.py
@@ -0,0 +1,10 @@
+a = input("Enter your strings with commas: \n")
+list1 = list(j.strip() for j in a.split(","))
+set1 = set(list1)
+d = {item: 0 for item in set1} 
+
+for item in list1:
+    if item in d:
+        d[item] += 1
+
+print(d.items())
